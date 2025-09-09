c7b87fdf82577076d1ce0db520a88d32ac913933
Thomas Carr <9591402+htcarr3@users.noreply.github.com>
2025-09-09 08:24:21 -0400

    generate basic auth models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_09_121414) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_09_122308) do
   create_table "chats", force: :cascade do |t|
     t.string "model_id"
     t.datetime "created_at", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_121414) do
     t.index ["tool_call_id"], name: "index_messages_on_tool_call_id"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "tool_calls", force: :cascade do |t|
     t.integer "message_id", null: false
     t.string "tool_call_id", null: false
@@ -42,6 +51,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_09_121414) do
     t.index ["tool_call_id"], name: "index_tool_calls_on_tool_call_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "messages", "chats"
+  add_foreign_key "sessions", "users"
   add_foreign_key "tool_calls", "messages"
 end
