c8445baf0c2d0fa79e57e11182280103be8d5316
Thomas Carr <9591402+htcarr3@users.noreply.github.com>
2025-09-14 11:15:37 -0400

    add new ruby_llm-generated models ui's

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,11 @@ Rails.application.routes.draw do
   resources :chats do
     resources :messages, only: [ :create ]
   end
+  resources :models, only: [ :index, :show ] do
+    collection do
+      post :refresh
+    end
+  end
   resources :raix_chats do
     resources :raix_messages, only: [ :create ]
   end
